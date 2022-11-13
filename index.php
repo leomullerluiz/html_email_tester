096f29fbca8a847458d8abb29642bbe9c00c115d
Leonan Müller <leonan@offmuller.com>
2022-11-13 15:11:29 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML E-Mail Tester</title>
+</head>
+<body>
+    <form action="send_email.php" method="POST">
+        <input type="text" name="email" placeholder="e-mail"/> <br>
+        <textarea name="message" id="" cols="30" rows="10"></textarea> <br>
+        <input type="submit" value="Test">
+
+
+    </form>
+</body>
+</html>
---
+++ send_email.php
@@ -0,0 +1,36 @@
+<?php
+    $to = $_POST['email'];
+    $subject = 'HTML E-mail Tester';
+    $from = 'htmlemailteste@lmuller.com.br';
+    // To send HTML mail, the Content-type header must be set
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+ 
+    // Create email headers
+    $headers .= 'From: '.$from."\r\n".
+    'Reply-To: '.$from."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+    $message = $_POST['message'];
+
+    // Sending email
+if(mail($to, $subject, $message, $headers)){
+    echo 'Your mail has been sent successfully.';
+} else{
+    echo 'Unable to send email. Please try again.';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <a href="./index.php">Home</a>
+</body>
+</html>
