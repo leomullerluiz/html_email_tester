4bb8c1832a4250caee9af1e4556f2ad8280eef57
Leonan Müller <leonan@offmuller.com>
2022-11-14 20:49:56 -0300

    add live preview

--- index.php
+++ index.php
@@ -4,15 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Develop HTML E-Mail Tester</title>
+    <title>[DEV] HTML E-Mail Tester</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <form action="send_email.php" method="POST">
         <input type="text" name="email" placeholder="e-mail"/> <br>
-        <textarea name="message" id="" cols="30" rows="10"></textarea> <br>
+        <textarea name="message" id="html" placeholder="HTML"></textarea> <br>
         <input type="submit" value="Test">
-
-
     </form>
+
+    <iframe id="code"></iframe>
+
+    <script type="text/javascript" src="js/app.js"></script>
+
 </body>
 </html>
